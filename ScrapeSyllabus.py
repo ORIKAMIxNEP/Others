6e621c7bc455f6d5903da07bcb2048f0c60644d9
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-17 03:22:25 +0900

    関数化

--- ScrapeSyllabus.py
+++ ScrapeSyllabus.py
@@ -6,40 +6,42 @@ option = Options()
 option.add_argument("--headless")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
 
-print("URLを入力してください")
-driver.get(input())
 
-subject = driver.find_element_by_xpath(
-    "//*[@id='wrap']/div[4]/div/div/div[1]/div/div[1]/h1").text
-wariaiLabel = driver.find_element_by_xpath(
-    "//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[1]").find_elements_by_tag_name("th")
-wariaiValue = driver.find_element_by_xpath(
-    "//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[2]").find_elements_by_tag_name("td")
+def ScrapeSyllabus(url):
+    driver.get(url)
 
-regularExam = "0"
-littleExam = "0"
-report = "0"
-attitude = "0"
-others = "0"
-annual = "false"
+    subject = driver.find_element_by_xpath(
+        "//*[@id='wrap']/div[4]/div/div/div[1]/div/div[1]/h1").text
+    wariaiLabel = driver.find_element_by_xpath(
+        "//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[1]").find_elements_by_tag_name("th")
+    wariaiValue = driver.find_element_by_xpath(
+        "//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[2]").find_elements_by_tag_name("td")
 
-count = 0
-for element in wariaiLabel:
-    if "試験" in element.text:
-        regularExam = wariaiValue[count].text
-    if "小テスト" == element.text:
-        littleExam = wariaiValue[count].text
-    if "レポート" in element.text or "ポートフォリオ" in element.text:
-        report = wariaiValue[count].text
-    if "発表" in element.text:
-        attitude = wariaiValue[count].text
-    if "その他" == element.text:
-        others = wariaiValue[count].text
-    count += 1
-if driver.find_element_by_xpath("//*[@id='MainContent_SubjectSyllabus_UpdatePanelSyllabus']/div/table/tbody/tr[6]/td[1]").text == "通年":
-    annual = "true"
+    regularExam = "0"
+    littleExam = "0"
+    report = "0"
+    attitude = "0"
+    others = "0"
+    annual = "false"
 
-print(",\n    {\n        subject: \"" + subject + "\",\n        regularExam: " + regularExam + ",\n        littleExam: " + littleExam +
-      ",\n        report: " + report + ",\n        attitude: " + attitude + ",\n        others: " + others + ",\n        annual: " + annual + "\n    }")
+    count = 0
+    for element in wariaiLabel:
+        if "試験" in element.text:
+            regularExam = wariaiValue[count].text
+        if "小テスト" == element.text:
+            littleExam = wariaiValue[count].text
+        if "レポート" in element.text or "ポートフォリオ" in element.text:
+            report = wariaiValue[count].text
+        if "発表" in element.text:
+            attitude = wariaiValue[count].text
+        if "その他" == element.text:
+            others = wariaiValue[count].text
+        count += 1
+    if driver.find_element_by_xpath("//*[@id='MainContent_SubjectSyllabus_UpdatePanelSyllabus']/div/table/tbody/tr[6]/td[1]").text == "通年":
+        annual = "true"
 
-driver.quit()
+    return ",\n    {\n        subject: \"" + subject + "\",\n        regularExam: " + regularExam + ",\n        littleExam: " + littleExam + ",\n        report: " + report + ",\n        attitude: " + attitude + ",\n        others: " + others + ",\n        annual: " + annual + "\n    }"
+
+
+def QuitDriver():
+    driver.quit()
