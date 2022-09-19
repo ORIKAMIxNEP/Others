6d0c3382deeabfa06945244673750bfa1418f02a
Taiki Orikami <65590892+ORIKAMIxNEP@users.noreply.github.com>
2022-09-19 17:43:28 +0900

    ファイル分割

--- web/script.js
+++ web/script.js
@@ -5,20 +5,7 @@ function paste() {
   });
 }
 
-async function ScrapeSyllabus(url) {
-  document.getElementById("result").innerHTML = await eel.CallScrapeSyllabus(
-    url
-  )();
-  navigator.clipboard.writeText(document.getElementById("result").innerHTML);
-  document.getElementById("message").style.visibility = "visible";
-  setTimeout(() => {
-    document.getElementById("url").value = "";
-    document.getElementById("message").style.visibility = "hidden";
-    document.getElementById("result").innerHTML = "";
-  }, 5000);
-}
-
-async function QuitDriver() {
+async function quitDriver() {
   await eel.CallQuitDriver()();
   window.open("", "_self").close();
 }
