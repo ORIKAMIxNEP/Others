b70b8b6961940a2250c90150174de8e90dda169d
ORIKAMI-NEP <ta.orikami@gmail.com>
2023-03-17 20:36:28 +0900

    headlessに変更

--- ScrapeSyllabus.py
+++ ScrapeSyllabus.py
@@ -3,7 +3,10 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = webdriver.ChromeOptions()
+options.headless = True
+driver = webdriver.Chrome(service=Service(
+    ChromeDriverManager().install()), options=options)
 
 
 def ScrapeSyllabus(url):
