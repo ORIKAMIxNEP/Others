448cbc61bf6656434cc5d4e588730f5feaae44d5
ORIKAMIxNEP <taiki.orikami@gmail.com>
2023-10-18 10:31:44 +0900

    format

--- ScrapeSyllabus.py
+++ ScrapeSyllabus.py
@@ -5,19 +5,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.headless = True
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=options
+)
 
 
 def ScrapeSyllabus(url):
     driver.get(url)
 
     subject = driver.find_element(
-        By.XPATH, value="//*[@id='wrap']/div[4]/div/div/div[1]/div/div[1]/h1").text
+        By.XPATH, value="//*[@id='wrap']/div[4]/div/div/div[1]/div/div[1]/h1"
+    ).text
     wariaiLabel = driver.find_element(
-        By.XPATH, value="//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[1]").find_elements(By.TAG_NAME, value="th")
+        By.XPATH, value="//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[1]"
+    ).find_elements(By.TAG_NAME, value="th")
     wariaiValue = driver.find_element(
-        By.XPATH, value="//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[2]").find_elements(By.TAG_NAME, value="td")
+        By.XPATH, value="//*[@id='MainContent_SubjectSyllabus_wariaiTable']/tbody/tr[2]"
+    ).find_elements(By.TAG_NAME, value="td")
 
     regularExam = "0"
     littleExam = "0"
@@ -39,10 +43,32 @@ def ScrapeSyllabus(url):
         if "その他" == element.text:
             others = wariaiValue[count].text
         count += 1
-    if driver.find_element(By.XPATH, value="//*[@id='MainContent_SubjectSyllabus_UpdatePanelSyllabus']/div/table/tbody/tr[6]/td[1]").text == "通年":
+    if (
+        driver.find_element(
+            By.XPATH,
+            value="//*[@id='MainContent_SubjectSyllabus_UpdatePanelSyllabus']/div/table/tbody/tr[6]/td[1]",
+        ).text
+        == "通年"
+    ):
         annual = "true"
 
-    return ",\n    {\n        subject: \"" + subject + "\",\n        regularExam: " + regularExam + ",\n        littleExam: " + littleExam + ",\n        report: " + report + ",\n        attitude: " + attitude + ",\n        others: " + others + ",\n        annual: " + annual + "\n    }"
+    return (
+        ',\n    {\n        subject: "'
+        + subject
+        + '",\n        regularExam: '
+        + regularExam
+        + ",\n        littleExam: "
+        + littleExam
+        + ",\n        report: "
+        + report
+        + ",\n        attitude: "
+        + attitude
+        + ",\n        others: "
+        + others
+        + ",\n        annual: "
+        + annual
+        + "\n    }"
+    )
 
 
 def QuitDriver():
